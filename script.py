29b226115217231b478a35843e945684f17bbf65
Luka <luka@arthaus.info>
2021-08-28 23:10:59 +0200

    generating first version of images

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 from PIL import Image, ImageChops, ImageFilter
 from os import walk, listdir
 from os.path import isfile
+import random
 
 
 # imageBg = "/mnt/c/Users/der_p/gimpProjects/FatPlat1/Backgrounds/020.png"
@@ -20,7 +21,7 @@ from os.path import isfile
 # bg.save("bg.png")
 
 def main():
-	providedPath = input("Provide the absolute path to your directory containing the images: ")
+	providedPath = input("Absolute path to your directory containing the images: ")
 
 	# check if last character is a slash
 	if providedPath[-1] != '/':
@@ -34,7 +35,77 @@ def main():
 	# ** equals to the power (possibilities^layers)
 	possibleImages = possibilities ** layers
 
-	print("layers: {} \npossibilities: {} \npossibleImages: {}".format(layers, possibilities, possibleImages ))
+	print("Index:")
+	print("layers: {} \npossibilities: {} \npossibleImages: {}\n".format(layers, possibilities, possibleImages ))
+
+	layerOrdering = setupLayerOrdering(layers, dirnames)
+	print("Layer list: {}\n".format(layerOrdering))
+
+	orderedImages = orderArray(layerOrdering, images)
+
+	imageName = input("How would you like to name your images? (Format will be NAME #ID) ")
+
+	randomOrAll = input("Generate all possible images (0) or specific count of random nonduplicates (1)? ")		
+
+	while int(randomOrAll) != 0 and int(randomOrAll) != 1:
+		randomOrAll = input("Generate all possible images (0) or specific count of random nonduplicates (1)? ")		
+	if int(randomOrAll) == 0:
+		# create all possible combinations of the image
+		createAllImgs(orderedImages)
+	else:
+		# create only a given amount of random images
+		imageAmount = input("How many images would you like to generate? ")
+		while int(imageAmount) <= 0 and int(imageAmount) >= possibleImages:
+			print("You are either choosing more images than possible or an impossible number!")
+			imageAmount = input("How many images would you like to generate? ")
+		createRandomImgs(imageAmount, orderedImages, imageName)
+
+
+# create a specific amount of random nonduplicate images
+def createRandomImgs(imageAmount, orderedImages, imageName):
+	imageAmount = int(imageAmount)
+	imagesArr = []
+	while len(imagesArr) < imageAmount:
+		innerList = []	
+		for layer in orderedImages:
+			randomImg = random.sample(layer["images"], 1)
+			innerList.append(Image.open(randomImg[0]["path"]))
+		imagesArr.append(innerList)
+	
+	id = 0
+	for image in imagesArr:
+		innerName = imageName + " #" + "{}".format(id)
+		layerAndSaveImg(image, innerName)
+		id = id + 1
+
+
+
+# create all possible images
+def createAllImgs(orderedImages):
+	pass
+
+
+# create ordered array
+def orderArray(layerOrdering, images):
+	orderedArray = []
+	for i in layerOrdering:
+		orderedArray.append(images[i])
+	return orderedArray
+
+
+# define order of layering
+def setupLayerOrdering(layers, dirnames):
+	for key in range(layers):
+		print("{}: {}".format(key, dirnames[key]))
+
+	layerOrdering = []
+	layerOrdering.append(int(input("You have {} layers. Which layer is the background? (ID): ".format(layers))))
+	processedLayers = layers - 1
+
+	while processedLayers > 0:
+		layerOrdering.append(int(input("Which Layer is next? (ID): ")))
+		processedLayers = processedLayers - 1
+	return layerOrdering
 
 
 # create list of directories and files of the provided path
@@ -44,6 +115,7 @@ def createDirectoryStructure(providedPath):
 		f.extend(filenames)
 		break
 	return dirnames
+
 
 # create subLists of the images in every directory
 def createImgList(dirnames, providedPath):
@@ -66,12 +138,13 @@ def createImgList(dirnames, providedPath):
 	return images
 
 # adds all the layers together in correct order and saves the image
-def layerImg(imgList):
+def layerAndSaveImg(imgList, imgName):
 	bg = imgList[0]
 	for item in imgList:
 		if item != imgList[0]:
 			bg.paste(item, (0,0), item)
-	bg.save("bg.png")
+	bg.save("generatedImgs/" + imgName + ".png")
+	print("Image {} generated".format(imgName))
 
 
 # init the main function
